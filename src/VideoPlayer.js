88e702efa691c46bc678cb23fc802f944f5021bc
fayzullabekchanov <faiz0587@gmail.com>
2024-10-15 19:07:17 +0500

    v1

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Sc5 from './Word/w7.mp4'
-import Sc5 from './Word/WordTable.mp4'
+// import Sc5 from './Word/WordTable.mp4'
 // import Sc5 from './scratch_mushuk_sichqon.mp4'
 // import Sc5 from './prip716.mp4'
 // import Sc5 from './Media1.mp4';
@@ -12,7 +12,7 @@ const VideoPlayer = () => {
             <tr>
                 <td><h2>Prip-720U</h2>
                     <video width="800" controls>
-                        <source src={Sc6} type="video/mp4"/>
+                        <source src="" type="video/mp4"/>
                     </video>
                 </td>
             </tr>
