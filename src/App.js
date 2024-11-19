b4bd88c9226e52da2720a29ea51b3535b977b66f
fayzullabekchanov <faiz0587@gmail.com>
2024-11-19 17:08:02 +0500

    GPA

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
+
 import React from 'react';
 import VideoPlayer from './VideoPlayer';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import G1f from './Word/w6.png';
-import G2f from './Word/w66.png';
-// import G4 from './Word/diag1.png';
+// import G1f from './Word/w4.png';
+// import G2f from './Word/w66.png';
+
+import G1f from './Excel/excel5.png';
+// import G1f from './dasturlar/blok.png';
+// import G21f from './Excel/excel1.png';
+// import G22f from './Excel/excel2.png';
+// import G2f from './Excel/excel1.1.png';
+// import G4f from './Excel/excel3.png';
 // import G5 from './Word/diag2.png';
-// import G4 from './Word/table.docx';
-// import G4f from './Word/w6.png';
+// import G4f from './Word/table3.png';
+// import G5f from './Word/tableru.png';
 // import G4f from './Word/table1.png';
 // import G5f from './Word/table2.png';
 // import G6f from './Word/table3.png';
 // import G1f from './mushuk_va_sichqon/1.png';
 // import G2f from './mushuk_va_sichqon/2.png';
-// import G4 from './AutoPlay_Media_Studio_v8.5.exe';
+import G4 from './Excel/GPA.xlsx';
+// import G4 from './APMS.exe';
+
+// import G1f from './Excel/excel3.png';
+// import G2f from './Excel/excel4.png';
+// import G3f from './Excel/excel1.png';
+// import G31f from './Excel/excel1.1.png';
+
+
+// import  G1 from './g1/1.jpg'
+// import  G2 from './g1/2.jpg'
+// import  G3 from './g1/4.jpg'
+// import  G4 from './g1/5.jpg'
+// import  G5 from './g1/7.jpg'
+// import  G6 from './g1/8.jpg'
+// import  G7 from './g1/9.jpg'
+// import  G8 from './g1/10.jpg'
+// import  G9 from './g1/11.jpg'
+// import  G10 from './g1/12.gif'
+// import  G11 from './g1/13.gif'
+// import  G12 from './g1/14.gif'
+// import  G13 from './g1/15.jpeg'
+// import  G14 from './g1/16.jpeg'
 
 
 function App() {
@@ -23,36 +52,44 @@ function App() {
             {/*<a href="https://cabinet.kiut.uz/" target="_blank">TEST</a>*/}
             {/*<a href="http://172.16.7.200" target="_blank">TEST</a>*/}
 
-            <img src={G1f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>
-            <br/>
-            shu jadvaldan diagrammalar yasang
-             <img src={G2f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>
-            {/*<img src={G3f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
+            {/*shu jadvaldan diagrammalar yasang*/}
+            {/*<br/> <br/> <br/>Variant 1 <br/>*/}
+            {/*Variant 1*/}
+            {/*<tr>*/}
+            {/*    <td><img src={G1} width={300} alt=" "/></td>*/}
+            {/*    <td><img src={G2} width={300} alt=" "/></td>*/}
+            {/*</tr>*/}
 
-            {/*<img src={G4f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
-            {/*<img src={G5f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
-            {/*<img src={G6f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
 
+            {/*<img src={G3} width={800} alt=" "/>*/}
+            {/*<img src={G4} width={800} alt=" "/>*/}
+            {/*<img src={G5} width={800} alt=" "/>*/}
+            {/*<img src={G6} width={800} alt=" "/>*/}
+            {/*<img src={G7} width={800} alt=" "/>*/}
+            {/*<img src={G8} width={800} alt=" "/>*/}
+            {/*<img src={G9} width={800} alt=" "/>*/}
+            {/*<img src={G10} width={800} alt=" "/>*/}
+            {/*<img src={G11} width={800} alt=" "/>*/}
+            {/*<img src={G12} width={800} alt=" "/>*/}
+            {/*<img src={G13} width={800} alt=" "/>*/}
+            {/*<img src={G14} width={800} alt=" "/>*/}
+            {/*Variant 1*/}
+            <img src={G1f} width={800} alt=" "/>
+
+            {/*Variant 2*/}
+            {/*<img src={G2f} width={800} alt=" "/>*/}
+            {/*Variant 3*/}
+            {/*<img src={G3f} width={800} alt=" "/>*/}
+            {/*<img src={G31f} width={800} alt=" "/>*/}
             {/*Variant 1 <img src={G6f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
-            {/*Variant 2 <img src={G5f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
-            {/*Variant 3 <img src={G4f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
 
-            {/*<img src={G4} width={900}*/}
-            {/*     style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
-            {/*-----------------*/}
-            {/*<img src={G5} width={900}*/}
-            {/*     style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
 
-            {/*<h1>Kuting tez orada chiqadi</h1>*/}
-
-                        {/*<a href={G4} download >            <button className="btn btn-success">Download</button>            </a>*/}
+            {/*<a href={G4} download > <button className="btn btn-success">Download</button>            </a>*/}
 
             {/*<a href="http://172.16.7.200" target="_blank">Rams yasash</a>*/}
             {/*<a href="https://www.fotor.com/images/create" target="_blank">Rams yasash</a>*/}
-            {/*<a href="https://heyzine.com/?hzpm=c1a4&gad_source=1&gclid=Cj0KCQjwr9m3BhDHARIsANut04Ywe31ZEDziO8clLUFZ1rxJRD8xZmvKsarpTntDWhm18fRe6ba0BeUaAvpfEALw_wcB"*/}
-            {/*   target="_blank">Kitob yasash</a>*/}
-            {/*<br/><br/><br/><br/><br/><br/>*/}
-            <VideoPlayer/>
+
+            {/*<VideoPlayer/>*/}
         </div>
     );
 }
--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,18 +1,20 @@
 import React from 'react';
-// import Sc5 from './Word/w7.mp4'
+// import Sc5 from './Word/Media1.mp4'
+// import Sc5 from './Excel/excel.mp4'
+// import Sc5 from './Excel/excel2.mp4'
 // import Sc5 from './Word/WordTable.mp4'
 // import Sc5 from './scratch_mushuk_sichqon.mp4'
 // import Sc5 from './prip716.mp4'
-// import Sc5 from './Media1.mp4';
+// import Sc5 from './Ertak_kitob.mp4';
 // import Sc6 from './PRIP720.mp4';
-// import Sc5 from './google.mp4';
+import Sc5 from './AMPS.mp4';
 const VideoPlayer = () => {
     return (
         <tr>
             <td>
-                <h2>Prip-720U</h2>
+                {/*<h2>Prip-720U</h2>*/}
                 <video width="800" controls>
-                    <source src="" type="video/mp4"/>
+                    <source src={Sc5} type="video/mp4"/>
                 </video>
             </td>
         </tr>
