c21d9179a9f2f84e275213ec94b5165105d14bfd
fayzullabekchanov <faiz0587@gmail.com>
2024-11-19 17:23:33 +0500

    GPA1.0

--- src/App.js
+++ src/App.js
@@ -15,47 +15,12 @@ function App() {
     return (
         <div className="App">
 
-            {/*<a href="https://cabinet.kiut.uz/" target="_blank">TEST</a>*/}
-            {/*<a href="http://172.16.7.200" target="_blank">TEST</a>*/}
 
-            {/*shu jadvaldan diagrammalar yasang*/}
-            {/*<br/> <br/> <br/>Variant 1 <br/>*/}
-            {/*Variant 1*/}
-            {/*<tr>*/}
-            {/*    <td><img src={G1} width={300} alt=" "/></td>*/}
-            {/*    <td><img src={G2} width={300} alt=" "/></td>*/}
-            {/*</tr>*/}
-
-
-            {/*<img src={G3} width={800} alt=" "/>*/}
-            {/*<img src={G4} width={800} alt=" "/>*/}
-            {/*<img src={G5} width={800} alt=" "/>*/}
-            {/*<img src={G6} width={800} alt=" "/>*/}
-            {/*<img src={G7} width={800} alt=" "/>*/}
-            {/*<img src={G8} width={800} alt=" "/>*/}
-            {/*<img src={G9} width={800} alt=" "/>*/}
-            {/*<img src={G10} width={800} alt=" "/>*/}
-            {/*<img src={G11} width={800} alt=" "/>*/}
-            {/*<img src={G12} width={800} alt=" "/>*/}
-            {/*<img src={G13} width={800} alt=" "/>*/}
-            {/*<img src={G14} width={800} alt=" "/>*/}
-            {/*Variant 1*/}
             <img src={G1f} width={800} alt=" "/>
 
-            {/*Variant 2*/}
-            {/*<img src={G2f} width={800} alt=" "/>*/}
-            {/*Variant 3*/}
-            {/*<img src={G3f} width={800} alt=" "/>*/}
-            {/*<img src={G31f} width={800} alt=" "/>*/}
-            {/*Variant 1 <img src={G6f} width={900}  style={{boxShadow: '1px 2px 10px black', borderRadius: '20px', backgroundColor: "white"}} alt=" "/>*/}
 
+            {/*<a href={G4} download > <button className="btn btn-success">Download</button></a>*/}
 
-            {/*<a href={G4} download > <button className="btn btn-success">Download</button>            </a>*/}
-
-            {/*<a href="http://172.16.7.200" target="_blank">Rams yasash</a>*/}
-            {/*<a href="https://www.fotor.com/images/create" target="_blank">Rams yasash</a>*/}
-
-            {/*<VideoPlayer/>*/}
         </div>
     );
 }
