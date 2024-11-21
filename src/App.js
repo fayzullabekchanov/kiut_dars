a26ba8bb9c5a0d6e4b88344229ca5acc384ade95
fayzullabekchanov <faiz0587@gmail.com>
2024-11-21 11:42:45 +0500

    GPA_Video

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 
 import React from 'react';
-// import VideoPlayer from './VideoPlayer';
+import VideoPlayer from './VideoPlayer';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
-
-import G1f from './Excel/excel5.png';
-
-
-
-
 
 function App() {
     return (
         <div className="App">
 
 
-            <img src={G1f} width={800} alt=" "/>
-
+            {/*<img src={G1f} width={800} alt=" "/>*/}
 
             {/*<a href={G4} download > <button className="btn btn-success">Download</button></a>*/}
-
+            <VideoPlayer/>
         </div>
+
     );
 }
 
--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-// import Sc5 from './AMPS.mp4';
+import Sc5 from './gpa.mp4';
 const VideoPlayer = () => {
     return (
         <tr>
             <td>
-                {/*<h2>Prip-720U</h2>*/}
                 <video width="800" controls>
                     <source src={Sc5} type="video/mp4"/>
                 </video>
