c3dde2642ad6807c4f818a8e51eff662515141c2
fayzullabekchanov <faiz0587@gmail.com>
2024-11-21 17:03:10 +0500

    GPA_Video1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import React from 'react';
 import VideoPlayer from './VideoPlayer';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
+import G1f from './excel5.png'
 function App() {
     return (
         <div className="App">
 
 
-            {/*<img src={G1f} width={800} alt=" "/>*/}
+            <img src={G1f} width={800} alt=" "/>
 
             {/*<a href={G4} download > <button className="btn btn-success">Download</button></a>*/}
+        <h2>Video</h2>
             <VideoPlayer/>
         </div>
 
