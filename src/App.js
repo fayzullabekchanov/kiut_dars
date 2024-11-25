363866f012df9fa91a466299aa301eee52fed055
fayzullabekchanov <faiz0587@gmail.com>
2024-11-25 12:46:17 +0500

    Scratch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 
 import React from 'react';
-import VideoPlayer from './VideoPlayer';
+// import VideoPlayer from './VideoPlayer';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import G1f from './excel5.png'
+import G1 from './rasm1.png'
+import G2 from './rasm2.png'
 function App() {
     return (
         <div className="App">
 
 
-            <img src={G1f} width={800} alt=" "/>
+            <img src={G1} width={800} alt=" "/>
+            <br/>
+            <img src={G2} width={800} alt=" "/>
 
             {/*<a href={G4} download > <button className="btn btn-success">Download</button></a>*/}
         <h2>Video</h2>
-            <VideoPlayer/>
+            {/*<VideoPlayer/>*/}
         </div>
 
     );
