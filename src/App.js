21ddd866f502ec4998fbd81e3e4a45be1d6ac19a
fayzullabekchanov <faiz0587@gmail.com>
2025-01-06 10:41:01 +0500

    word13

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import VideoPlayer from './VideoPlayer';
+// import VideoPlayer from './VideoPlayer';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import G1 from './pstat.png'
+import G1 from './w3.png'
 
 function App() {
     return (
         <div className="App">
 
 
-            {/*<img src={G1} width={800} alt=" "/>*/}
+            <img src={G1} width={800} alt=" "/>
             {/*<a href={G1} download > <button className="btn btn-success">Download</button></a>*/}
-           <br/>
-            <h2>Video</h2>
-            <VideoPlayer/>
+           {/*<br/>*/}
+           {/* <h2>Video</h2>*/}
+           {/* <VideoPlayer/>*/}
         </div>
 
     );
