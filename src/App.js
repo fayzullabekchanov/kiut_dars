459bb9dc2232cbb36a53637f87738e9fef9b169e
fayzullabekchanov <faiz0587@gmail.com>
2025-01-08 12:00:08 +0500

    word13_vide

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import VideoPlayer from './VideoPlayer';
+// import VideoPlayer from './VideoPlayer';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import G1 from './w3.png'
+// import G1 from './w3.png'
+import G2 from './excel5.png'
+import G3 from './excel5j.png'
 
 function App() {
     return (
         <div className="App">
 
 
-            <img src={G1} width={800} alt=" "/>
+            {/*<img src={G1} width={800} alt=" "/>*/}
+            <img src={G2} width={800} alt=" "/>
+            <br/>
+            <h2>yechilish ketma ketligi</h2>
+            <img src={G3} width={800} alt=" "/>
             {/*<a href={G1} download > <button className="btn btn-success">Download</button></a>*/}
-           <br/>
-            <h2>Video qo`llanma</h2>
-            <VideoPlayer/>
+            <br/>
+            {/*<h2>Video qo`llanma</h2>*/}
+            {/*<VideoPlayer/>*/}
         </div>
 
     );
