659446d675c170dc430a89aa8e554bdc4f3b28e2
fayzullabekchanov <faiz0587@gmail.com>
2025-02-17 14:23:41 +0500

    G1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
- import VideoPlayer from './VideoPlayer';
+// import VideoPlayer from './VideoPlayer';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
- // import G1 from './w3.png'
-import G2 from './excel5.png'
-import G3 from './excel5j.png'
+import G1 from './g1/1.jpg'
+import G2 from './g1/2.jpg'
+import G3 from './g1/3.jpg'
+import G4 from './g1/4.jpg'
+import G5 from './g1/5.jpg'
+import G7 from './g1/7.jpg'
+import G8 from './g1/8.jpg'
+import G9 from './g1/9.jpg'
+import G10 from './g1/10.jpg'
+import G11 from './g1/11.jpg'
+import G12 from './g1/12.gif'
+import G13 from './g1/13.gif'
+import G14 from './g1/14.gif'
+import G15 from './g1/15.jpeg'
+import G16 from './g1/16.jpeg'
+import G17 from './g1/17.jpg'
+import G18 from './g1/18.png'
 
 function App() {
     return (
         <div className="App">
 
 
-       {/*<img src={G1} width={800} alt=" "/>*/}
+            <img src={G1} width={800} alt=" "/>
             <img src={G2} width={800} alt=" "/>
-            <br/>
-            <h2>yechilish ketma ketligi</h2>
             <img src={G3} width={800} alt=" "/>
-       {/*     <a href={G1} download > <button className="btn btn-success">Download</button></a>*/}
-            {/*<br/>*/}
-         {/*<h2>Video qo`llanma</h2>*/}
-         {/*   <VideoPlayer/>*/}
+            <img src={G4} width={800} alt=" "/>
+            <img src={G5} width={800} alt=" "/>
+            <img src={G7} width={800} alt=" "/>
+            <img src={G8} width={800} alt=" "/>
+            <img src={G9} width={800} alt=" "/>
+            <img src={G10} width={800} alt=" "/>
+            <img src={G11} width={800} alt=" "/>
+            <img src={G12} width={800} alt=" "/>
+            <img src={G13} width={800} alt=" "/>
+            <img src={G14} width={800} alt=" "/>
+            <img src={G15} width={800} alt=" "/>
+            <img src={G16} width={800} alt=" "/>
+            <img src={G17} width={800} alt=" "/>
+            <img src={G18} width={800} alt=" "/>
+
+
+            {/*     <a href={G1} download > <button className="btn btn-success">Download</button></a>*/}
+
         </div>
 
     );
