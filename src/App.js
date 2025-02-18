5816ee8e7b407a5c2ee67909e6b1c260d8efb503
fayzullabekchanov <faiz0587@gmail.com>
2025-02-18 15:01:48 +0500

    scratch

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import G1 from './g1/1.jpg'
-import G2 from './g1/2.jpg'
-import G3 from './g1/3.jpg'
-import G4 from './g1/4.jpg'
-import G5 from './g1/5.jpg'
-import G7 from './g1/7.jpg'
-import G8 from './g1/8.jpg'
-import G9 from './g1/9.jpg'
-import G10 from './g1/10.jpg'
-import G11 from './g1/11.jpg'
-import G12 from './g1/12.gif'
-import G13 from './g1/13.gif'
-import G14 from './g1/14.gif'
-import G15 from './g1/15.jpeg'
-import G16 from './g1/16.jpeg'
-import G17 from './g1/17.jpg'
-import G18 from './g1/18.png'
+// import G1 from './g1/1.jpg'
+// import G2 from './g1/2.jpg'
+// import G3 from './g1/3.jpg'
+// import G4 from './g1/4.jpg'
+// import G5 from './g1/5.jpg'
+// import G7 from './g1/7.jpg'
+// import G8 from './g1/8.jpg'
+// import G9 from './g1/9.jpg'
+// import G10 from './g1/10.jpg'
+// import G11 from './g1/11.jpg'
+// import G12 from './g1/12.gif'
+// import G13 from './g1/13.gif'
+// import G14 from './g1/14.gif'
+// import G15 from './g1/15.jpeg'
+// import G16 from './g1/16.jpeg'
+// import G17 from './g1/17.jpg'
+// import G18 from './g1/18.png'
+
+
+import G1 from './1.png'
+import G2 from './2.png'
 
 function App() {
     return (
@@ -28,21 +32,22 @@ function App() {
 
             <img src={G1} width={800} alt=" "/>
             <img src={G2} width={800} alt=" "/>
-            <img src={G3} width={800} alt=" "/>
-            <img src={G4} width={800} alt=" "/>
-            <img src={G5} width={800} alt=" "/>
-            <img src={G7} width={800} alt=" "/>
-            <img src={G8} width={800} alt=" "/>
-            <img src={G9} width={800} alt=" "/>
-            <img src={G10} width={800} alt=" "/>
-            <img src={G11} width={800} alt=" "/>
-            <img src={G12} width={800} alt=" "/>
-            <img src={G13} width={800} alt=" "/>
-            <img src={G14} width={800} alt=" "/>
-            <img src={G15} width={800} alt=" "/>
-            <img src={G16} width={800} alt=" "/>
-            <img src={G17} width={800} alt=" "/>
-            <img src={G18} width={800} alt=" "/>
+            {/*<img src={G2} width={800} alt=" "/>*/}
+            {/*<img src={G3} width={800} alt=" "/>*/}
+            {/*<img src={G4} width={800} alt=" "/>*/}
+            {/*<img src={G5} width={800} alt=" "/>*/}
+            {/*<img src={G7} width={800} alt=" "/>*/}
+            {/*<img src={G8} width={800} alt=" "/>*/}
+            {/*<img src={G9} width={800} alt=" "/>*/}
+            {/*<img src={G10} width={800} alt=" "/>*/}
+            {/*<img src={G11} width={800} alt=" "/>*/}
+            {/*<img src={G12} width={800} alt=" "/>*/}
+            {/*<img src={G13} width={800} alt=" "/>*/}
+            {/*<img src={G14} width={800} alt=" "/>*/}
+            {/*<img src={G15} width={800} alt=" "/>*/}
+            {/*<img src={G16} width={800} alt=" "/>*/}
+            {/*<img src={G17} width={800} alt=" "/>*/}
+            {/*<img src={G18} width={800} alt=" "/>*/}
 
 
             {/*     <a href={G1} download > <button className="btn btn-success">Download</button></a>*/}
