58e7df6d09fcdcb5aba183a80c689f1b1f0e339b
fayzullabekchanov <faiz0587@gmail.com>
2025-04-09 13:59:48 +0500

    kitob

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import VideoPlayer from './VideoPlayer';
+// import VideoPlayer from './VideoPlayer';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
+import  Im1 from './1.png'
+import  Im2 from './2.png'
+import  Im3 from './3.png'
+import  Im4 from './4.png'
 
 function App() {
     return (
         <div className="App">
 
-
+            <h1>1-rasm</h1>
+            <img src={Im1} alt=""/>
+            <br/>
+            <h1>2-rasm</h1>
+            <img src={Im2} alt=""/>
+            <img src={Im3} alt=""/>
+            <h1>3-rasm</h1>
+            <br/>
+            <img src={Im4} alt=""/>
 
 
 
             {/*   <a href={G1} download > <button className="btn btn-success">Download</button></a> */}
-<VideoPlayer/>
-            <a href="https://perchance.org/ai-text-to-image-generator" target="_blank">Rasm yaratish</a>
-            <a href="https://heyzine.com" target="_blank"> Kitob yaratish</a>
-            <a href="https://chatgpt.com/" target="_blank">AI chat</a>
-            <a href="https://grok.com/" target="_blank">AI chat</a>
-        </div>
+{/*<VideoPlayer/>*/}
+             </div>
 
     );
 }
