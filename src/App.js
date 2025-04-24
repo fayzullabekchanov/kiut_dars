3b8426d9aafbf7db92cd80cd6b23d21ceb89aab2
fayzullabekchanov <faiz0587@gmail.com>
2025-04-24 14:35:50 +0500

    scratch1

--- src/App.js
+++ src/App.js
@@ -5,22 +5,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import  Im1 from './1.png'
 import  Im2 from './2.png'
-import  Im3 from './3.png'
-import  Im4 from './4.png'
+
 
 function App() {
     return (
         <div className="App">
 
             <h1>1-rasm</h1>
-            <img src={Im1} alt=""/>
+            <img src={Im1} height={800} alt=""/>
             <br/>
             <h1>2-rasm</h1>
-            <img src={Im2} alt=""/>
-            <img src={Im3} alt=""/>
-            <h1>3-rasm</h1>
-            <br/>
-            <img src={Im4} alt=""/>
+            <img src={Im2} height={800} alt=""/>
 
 
 
