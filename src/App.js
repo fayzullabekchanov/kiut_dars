4d8e6380eb790ed51012811bc801a0a1a76781ac
fayzullabekchanov <faiz0587@gmail.com>
2025-05-01 17:20:45 +0500

    labirint

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import  Im1 from './1.png'
-import  Im2 from './2.png'
+import  Im1 from './rasm1.png'
+import  Im2 from './rasm2.png'
+import  Im3 from './fon 1.svg'
+import  Im4 from './fon 2.svg'
+import  Im5 from './fon 3.svg'
+import  Im6 from './fon 4.svg'
+
 
 
 function App() {
@@ -19,7 +24,10 @@ function App() {
 
 
 
-            {/*   <a href={G1} download > <button className="btn btn-success">Download</button></a> */}
+               <a href={Im3} download > <button className="btn btn-success">Fon1</button></a>
+               <a href={Im4} download > <button className="btn btn-success">Fon2</button></a>
+               <a href={Im5} download > <button className="btn btn-success">Fon3</button></a>
+               <a href={Im6} download > <button className="btn btn-success">Fon4</button></a>
 {/*<VideoPlayer/>*/}
              </div>
 
