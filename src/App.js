f60dac0dfb0b0a99748d758e701560a45554d8e6
fayzullabekchanov <faiz0587@gmail.com>
2025-05-30 15:47:11 +0500

    TayorAPMS

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import VideoPlayer from './VideoPlayer';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import  Im1 from './rasm1.png'
-import  Im2 from './rasm2.png'
-import  Im3 from './fon 1.svg'
-import  Im4 from './fon 2.svg'
-import  Im5 from './fon 3.svg'
-import  Im6 from './fon 4.svg'
-import  Im7 from './qr-code.gif'
+
 
 
 
@@ -17,20 +11,9 @@ function App() {
     return (
         <div className="App">
 
-            <h1>1-rasm</h1>
-            <img src={Im1} height={800} alt=""/>
-            <br/>
-            <h1>2-rasm</h1>
-            <img src={Im2} height={800} alt=""/>
 
-
-
-               <a href={Im3} download > <button className="btn btn-success">Fon1</button></a>
-               <a href={Im4} download > <button className="btn btn-success">Fon2</button></a>
-               <a href={Im5} download > <button className="btn btn-success">Fon3</button></a>
-               <a href={Im6} download > <button className="btn btn-success">Fon4</button></a>
             <h1>Video qo`llanma </h1>
-            <img src={Im7} height={800} alt=""/>
+          <VideoPlayer/>
              </div>
 
     );
--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,16 +1,16 @@
-// import React from 'react';
-//
-// import Sc5 from './paste1.mp4';
-// const VideoPlayer = () => {
-//     return (
-//         <tr>
-//             <td>
-//                 <video width="800" controls>
-//                     <source src={Sc5} type="video/mp4"/>
-//                 </video>
-//             </td>
-//         </tr>
-//     );
-// };
-//
-// export default VideoPlayer;
+import React from 'react';
+
+import Sc5 from './App.mp4';
+const VideoPlayer = () => {
+    return (
+        <tr>
+            <td>
+                <video width="800" controls>
+                    <source src={Sc5} type="video/mp4"/>
+                </video>
+            </td>
+        </tr>
+    );
+};
+
+export default VideoPlayer;
