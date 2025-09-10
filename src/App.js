0bd71ef87b333475ea45591cb9e64c12d913b018
fayzullabekchanov <faiz0587@gmail.com>
2025-09-10 17:01:25 +0500

    W13

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import Worddagi1 from './1.png'
-import Worddagi2 from './2.png'
-import Worddagi3 from './3.png'
+import Worddagi1 from './w3.png'
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -15,8 +14,6 @@ function App() {
         <div className="App">
 
             <img src={Worddagi1} alt=""/>
-            <img src={Worddagi2} alt=""/>
-            <img src={Worddagi3} width={1000} alt=""/>
 
              </div>
 
