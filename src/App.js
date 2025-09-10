0bb0669d84350089bc69580b615f9401708ee6d3
fayzullabekchanov <faiz0587@gmail.com>
2025-09-10 17:28:28 +0500

    W13_Video bilan

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Worddagi1 from './w3.png'
-
+import MyVideo from './w13.mp4';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -14,7 +14,11 @@ function App() {
         <div className="App">
 
             <img src={Worddagi1} alt=""/>
-
+            ------------------------Video Qo`llanma------------------------
+            <video  controls>
+                <source src={MyVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
              </div>
 
     );
