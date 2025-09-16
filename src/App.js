34d46d28129e7a3ce3e44f751d9757054d6dee79
fayzullabekchanov <faiz0587@gmail.com>
2025-09-16 12:50:34 +0500

    W13_V2_V4_V5_Video bilan

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import Worddagi1 from './w3.png'
+import Worddagi1 from './v2.jpg'
+import Worddagi2 from './v4.jpg'
+import Worddagi3 from './v5.jpg'
+import Worddagi4 from './w3.png'
 import MyVideo from './w13.mp4';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -14,6 +17,12 @@ function App() {
         <div className="App">
 
             <img src={Worddagi1} alt=""/>
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi2} alt=""/>
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi3} alt=""/>
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi4} alt=""/>
             ------------------------Video Qo`llanma------------------------
             <video  controls>
                 <source src={MyVideo} type="video/mp4" />
