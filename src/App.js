d94f31c8bef061c4ec2ddc3e2451ffcabbf95d85
fayzullabekchanov <faiz0587@gmail.com>
2025-09-22 18:03:17 +0500

    varizntlar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 // import Worddagi1 from './jadval.png'
 // import Worddagi2 from './wtable.png'
-import Worddagi1 from './v2.jpg'
-import Worddagi2 from './v4.jpg'
-import Worddagi3 from './v5.jpg'
+// import Worddagi1 from './v2.jpg'
+// import Worddagi2 from './v4.jpg'
+// import Worddagi3 from './v5.jpg'
 import Worddagi4 from './w3.png'
 import MyVideo from './w13.mp4';
 
@@ -18,12 +18,12 @@ function App() {
     return (
         <div className="App">
 
-            <img src={Worddagi1} alt=""/>
-            ------------------------   ------------------------   ------------------------   ------------------------
-            <img src={Worddagi2} alt=""/>
-            ------------------------   ------------------------   ------------------------   ------------------------
-            <img src={Worddagi3} alt=""/>
-            ------------------------   ------------------------   ------------------------   ------------------------
+            {/*<img src={Worddagi1} alt=""/>*/}
+            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
+            {/*<img src={Worddagi2} alt=""/>*/}
+            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
+            {/*<img src={Worddagi3} alt=""/>*/}
+            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
             <img src={Worddagi4} alt=""/>
             ------------------------Video Qo`llanma------------------------
             <video  controls>
