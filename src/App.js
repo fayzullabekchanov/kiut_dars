31c362e9cabe00b24592b2c4a1a74215450adfaa
fayzullabekchanov <faiz0587@gmail.com>
2025-09-22 15:05:05 +0500

    varizntlar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import Worddagi1 from './jadval.png'
-import Worddagi2 from './wtable.png'
-// import Worddagi2 from './v4.jpg'
-// import Worddagi3 from './v5.jpg'
-// import Worddagi4 from './w3.png'
-// import MyVideo from './w13.mp4';
+// import Worddagi1 from './jadval.png'
+// import Worddagi2 from './wtable.png'
+import Worddagi1 from './v2.jpg'
+import Worddagi2 from './v4.jpg'
+import Worddagi3 from './v5.jpg'
+import Worddagi4 from './w3.png'
+import MyVideo from './w13.mp4';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -20,15 +21,15 @@ function App() {
             <img src={Worddagi1} alt=""/>
             ------------------------   ------------------------   ------------------------   ------------------------
             <img src={Worddagi2} alt=""/>
-            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
-            {/*<img src={Worddagi3} alt=""/>*/}
-            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
-            {/*<img src={Worddagi4} alt=""/>*/}
-            {/*------------------------Video Qo`llanma------------------------*/}
-            {/*<video  controls>*/}
-            {/*    <source src={MyVideo} type="video/mp4" />*/}
-            {/*    Your browser does not support the video tag.*/}
-            {/*</video>*/}
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi3} alt=""/>
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi4} alt=""/>
+            ------------------------Video Qo`llanma------------------------
+            <video  controls>
+                <source src={MyVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
              </div>
 
     );
