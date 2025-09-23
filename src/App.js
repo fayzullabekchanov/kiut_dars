18f61854e3bb99fa2d25e243226c31e55163fd90
fayzullabekchanov <faiz0587@gmail.com>
2025-09-23 12:53:47 +0500

    varizntlar_jadval

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
-// import Worddagi1 from './jadval.png'
-// import Worddagi2 from './wtable.png'
-// import Worddagi1 from './v2.jpg'
-// import Worddagi2 from './v4.jpg'
-// import Worddagi3 from './v5.jpg'
-import Worddagi4 from './w3.png'
-import MyVideo from './w13.mp4';
+import Worddagi1 from './jadval.png'
+import Worddagi2 from './jadval1.png'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -18,18 +13,18 @@ function App() {
     return (
         <div className="App">
 
-            {/*<img src={Worddagi1} alt=""/>*/}
-            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
-            {/*<img src={Worddagi2} alt=""/>*/}
+            <img src={Worddagi1} alt=""/>
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi2} alt="" width={900}/>
             {/*------------------------   ------------------------   ------------------------   ------------------------*/}
             {/*<img src={Worddagi3} alt=""/>*/}
             {/*------------------------   ------------------------   ------------------------   ------------------------*/}
-            <img src={Worddagi4} alt=""/>
-            ------------------------Video Qo`llanma------------------------
-            <video  controls>
-                <source src={MyVideo} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            {/*<img src={Worddagi4} alt=""/>*/}
+            {/*------------------------Video Qo`llanma------------------------*/}
+            {/*<video  controls>*/}
+            {/*    <source src={MyVideo} type="video/mp4" />*/}
+            {/*    Your browser does not support the video tag.*/}
+            {/*</video>*/}
              </div>
 
     );
