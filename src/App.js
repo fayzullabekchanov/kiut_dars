72893285923024ba392c94e5e55f8ca422a2de68
fayzullabekchanov <faiz0587@gmail.com>
2025-09-24 18:04:08 +0500

    Jadval_bittada_uchta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 
-import Worddagi1 from './jadval1.png'
-// import Worddagi1 from './v2.jpg'
-// import Worddagi2 from './v3.jpg'
-// import Worddagi3 from './v4.jpg'
-// import Worddagi4 from './v5.jpg'
+// import Worddagi1 from './jadval1.png'
+import Worddagi1 from './v2.jpg'
+import Worddagi2 from './v3.jpg'
+import Worddagi3 from './v4.jpg'
+import Worddagi4 from './v5.jpg'
+import MyVideo from './w13.mp4'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -16,19 +17,19 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            {/*<h1>Faqat bitta variantini tanlang</h1>*/}
+            <h1>Faqat bitta variantini tanlang</h1>
             <img src={Worddagi1} alt=""/>
-            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
-            {/*<img src={Worddagi2} alt=""/>*/}
-            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
-            {/*<img src={Worddagi3} alt=""/>*/}
-            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
-            {/*<img src={Worddagi4} alt=""/>*/}
-            {/*------------------------Video Qo`llanma------------------------*/}
-            {/*<video  controls>*/}
-            {/*    <source src={MyVideo} type="video/mp4" />*/}
-            {/*    Your browser does not support the video tag.*/}
-            {/*</video>*/}
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi2} alt=""/>
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi3} alt=""/>
+            ------------------------   ------------------------   ------------------------   ------------------------
+            <img src={Worddagi4} alt=""/>
+            ------------------------Video Qo`llanma------------------------
+            <video  controls>
+                <source src={MyVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
              </div>
 
     );
