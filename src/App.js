50310a158daca7e0bc5bb121b65a7856930df1ce
fayzullabekchanov <faiz0587@gmail.com>
2025-09-25 10:00:36 +0500

    Variant_13

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 
 // import Worddagi1 from './jadval1.png'
-import Worddagi1 from './v2.jpg'
-import Worddagi2 from './v3.jpg'
-import Worddagi3 from './v4.jpg'
-import Worddagi4 from './v5.jpg'
+// import Worddagi1 from './v2.jpg'
+// import Worddagi2 from './v3.jpg'
+// import Worddagi3 from './v4.jpg'
+// import Worddagi4 from './v5.jpg'
+import Worddagi4 from './w3.png'
 import MyVideo from './w13.mp4'
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -17,13 +18,13 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>Faqat bitta variantini tanlang</h1>
-            <img src={Worddagi1} alt=""/>
-            ------------------------   ------------------------   ------------------------   ------------------------
-            <img src={Worddagi2} alt=""/>
-            ------------------------   ------------------------   ------------------------   ------------------------
-            <img src={Worddagi3} alt=""/>
-            ------------------------   ------------------------   ------------------------   ------------------------
+            {/*<h1>Faqat bitta variantini tanlang</h1>*/}
+            {/*<img src={Worddagi1} alt=""/>*/}
+            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
+            {/*<img src={Worddagi2} alt=""/>*/}
+            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
+            {/*<img src={Worddagi3} alt=""/>*/}
+            {/*------------------------   ------------------------   ------------------------   ------------------------*/}
             <img src={Worddagi4} alt=""/>
             ------------------------Video Qo`llanma------------------------
             <video  controls>
