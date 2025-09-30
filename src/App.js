828c2fdafce9b1fbafba1be359b1a4e3a3eb05c5
fayzullabekchanov <faiz0587@gmail.com>
2025-09-30 12:57:04 +0500

    RAsm_jadval

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 
-import Worddagi1 from './w3.png'
+import Worddagi1 from './ram_jadval1.png'
 
-import MyVideo from './w13.mp4'
+// import MyVideo from './w13.mp4'
 
 //
-// import Worddagi2 from './ram_jadval2.png'
-// import Worddagi3 from './ram_jadval3.png'
-// import Worddagi4 from './ram_jadval4.png'
-// import Worddagi5 from './ram_jadval5.png'
-// import MData from './rasmlar.zip'
+import Worddagi2 from './ram_jadval2.png'
+import Worddagi3 from './ram_jadval3.png'
+import Worddagi4 from './ram_jadval4.png'
+import Worddagi5 from './ram_jadval5.png'
+import MData from './rasmlar.zip'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -23,35 +23,35 @@ function App() {
         <div className="App">
             {/*<h1>Faqat bitta variantini tanlang</h1>*/}
             <img src={Worddagi1} alt=""/>
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi2} alt="" />*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi3} alt=""/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi4} alt=""/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi5} alt=""/>*/}
-            {/*/!*------------------------Rasmlarni yuklab oling------------------------*!/*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<br/>*/}
-            {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
-            <video  controls>
-                <source src={MyVideo} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <br/>
+            <br/>
+            <br/>
+            ------------------------   ------------------------
+            <img src={Worddagi2} alt="" />
+            <br/>
+            <br/>
+            <br/>
+            ------------------------   ------------------------
+            <img src={Worddagi3} alt=""/>
+            <br/>
+            <br/>
+            <br/>
+            ------------------------   ------------------------
+            <img src={Worddagi4} alt=""/>
+            <br/>
+            <br/>
+            <br/>
+            ------------------------   ------------------------
+            <img src={Worddagi5} alt=""/>
+            {/*------------------------Rasmlarni yuklab oling------------------------*/}
+            <br/>
+            <br/>
+            <br/>
+            <a href={MData}>Rasmlarni yuklab oling</a>
+            {/*<video  controls>*/}
+            {/*    <source src={MyVideo} type="video/mp4" />*/}
+            {/*    Your browser does not support the video tag.*/}
+            {/*</video>*/}
              </div>
 
     );
