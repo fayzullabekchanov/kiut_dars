1eae62f16ee43d19789312cdfd426b7212e75d26
fayzullabekchanov <faiz0587@gmail.com>
2025-10-06 15:04:03 +0500

    Word_1.0

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
 
-// import Worddagi1 from './ram_jadval1.png'
-
-// import MyVideo from './w13.mp4'
-// import Worddagi2 from './ram_jadval2.png'
-// import Worddagi3 from './ram_jadval3.png'
-// import Worddagi4 from './ram_jadval4.png'
-// import Worddagi5 from './ram_jadval5.png'
+import Worddagi1 from './ram_jadval1.png'
+import Worddagi2 from './ram_jadval2.png'
+import Worddagi3 from './ram_jadval3.png'
+import Worddagi4 from './ram_jadval4.png'
+import Worddagi5 from './ram_jadval5.png'
 
 
-import Worddagi2 from './excel1.png'
-import Worddagi3 from './excel1.1.png'
-// import Worddagi4 from './v4.jpg'
-// import Worddagi5 from './v5.jpg'
-// import MData from './rasmlar.zip'
+
+import MData from './rasmlar.zip'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -28,22 +23,23 @@ function App() {
 
 
         <div className="App">
+
             {/*<h1>Faqat bitta variantini tanlang</h1>*/}
-            {/*<img src={Worddagi1} alt=""/>*/}
-            {/*------------------------   ------------------------*/}
+            <img src={Worddagi1} alt=""/>
+            ------------------------   ------------------------
             <img src={Worddagi2} alt="" />
 
-            {/*------------------------   ------------------------*/}
+            ------------------------   ------------------------
             <img src={Worddagi3} alt=""/>
 
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi4} alt=""/>*/}
+            ------------------------   ------------------------
+            <img src={Worddagi4} alt=""/>
 
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi5} alt=""/>*/}
+            ------------------------   ------------------------
+            <img src={Worddagi5} alt=""/>
             {/*------------------------Rasmlarni yuklab oling------------------------*/}
 
-            {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
+            <a href={MData}>Rasmlarni yuklab oling</a>
             {/*<video  controls>*/}
             {/*    <source src={MyVideo} type="video/mp4" />*/}
             {/*    Your browser does not support the video tag.*/}
