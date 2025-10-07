e5e7525b0e0d7b05695787088dbbea43414ea431
fayzullabekchanov <faiz0587@gmail.com>
2025-10-07 13:05:24 +0500

    Excel_1.0

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ import React from 'react';
 // import Worddagi5 from './ram_jadval5.png'
 
 
-import Worddagi1 from './v2.jpg'
-import Worddagi2 from './v3.jpg'
-import Worddagi3 from './v4.jpg'
-import Worddagi4 from './v5.jpg'
-import MyVideo from './w13.mp4'
+// import Worddagi1 from './v2.jpg'
+// import Worddagi2 from './v3.jpg'
+// import Worddagi3 from './v4.jpg'
+// import Worddagi4 from './v5.jpg'
+// import MyVideo from './w13.mp4'
 
-import MData from './rasmlar.zip'
+// import MData from './rasmlar.zip'
+
+import Worddagi1 from './excel1.png'
+import Worddagi2 from './excel1.1.png'
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -34,21 +38,23 @@ function App() {
             ------------------------   ------------------------
             <img src={Worddagi2} alt="" />
 
-            ------------------------   ------------------------
-            <img src={Worddagi3} alt=""/>
+            {/*------------------------   ------------------------*/}
+            {/*<img src={Worddagi3} alt=""/>*/}
 
-            ------------------------   ------------------------
-            <img src={Worddagi4} alt=""/>
+            {/*------------------------   ------------------------*/}
+            {/*<img src={Worddagi4} alt=""/>*/}
 
             {/*------------------------   ------------------------*/}
             {/*<img src={Worddagi5} alt=""/>*/}
             {/*------------------------Rasmlarni yuklab oling------------------------*/}
 
             {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
-            <video  controls>
-                <source src={MyVideo} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+
+            {/*<video  controls>*/}
+            {/*    <source src={MyVideo} type="video/mp4" />*/}
+            {/*    Your browser does not support the video tag.*/}
+            {/*</video>*/}
+
              </div>
 
 
