7002c8820ad8f2f02a835c4c333e76f5b67fc83a
fayzullabekchanov <faiz0587@gmail.com>
2025-10-07 18:02:47 +0500

    Word_1.4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 
-// import Worddagi1 from './ram_jadval1.png'
-// import Worddagi2 from './ram_jadval2.png'
-// import Worddagi3 from './ram_jadval3.png'
-// import Worddagi4 from './ram_jadval4.png'
-// import Worddagi5 from './ram_jadval5.png'
+import Worddagi1 from './ram_jadval1.png'
+import Worddagi2 from './ram_jadval2.png'
+import Worddagi3 from './ram_jadval3.png'
+import Worddagi4 from './ram_jadval4.png'
+import Worddagi5 from './ram_jadval5.png'
 
-
+//
 // import Worddagi1 from './v2.jpg'
 // import Worddagi2 from './v3.jpg'
 // import Worddagi3 from './v4.jpg'
 // import Worddagi4 from './v5.jpg'
 // import MyVideo from './w13.mp4'
 
-// import MData from './rasmlar.zip'
-
-import Worddagi1 from './excel1.png'
-import Worddagi2 from './excel1.1.png'
+import MData from './rasmlar.zip'
+//
+// import Worddagi1 from './excel1.png'
+// import Worddagi2 from './excel1.1.png'
 
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -38,17 +38,17 @@ function App() {
             ------------------------   ------------------------
             <img src={Worddagi2} alt="" />
 
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi3} alt=""/>*/}
+            ------------------------   ------------------------
+            <img src={Worddagi3} alt=""/>
 
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi4} alt=""/>*/}
+            ------------------------   ------------------------
+            <img src={Worddagi4} alt=""/>
 
-            {/*------------------------   ------------------------*/}
-            {/*<img src={Worddagi5} alt=""/>*/}
-            {/*------------------------Rasmlarni yuklab oling------------------------*/}
+            ------------------------   ------------------------
+            <img src={Worddagi5} alt=""/>
+            ------------------------------------------------
 
-            {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
+            <a href={MData}>Rasmlarni yuklab oling</a>
 
             {/*<video  controls>*/}
             {/*    <source src={MyVideo} type="video/mp4" />*/}
