df6cd3035b9ac0e67e35e2e3e28234b5200e96cd
fayzullabekchanov <faiz0587@gmail.com>
2025-10-07 11:04:39 +0500

    Word_1.3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 
-import Worddagi1 from './ram_jadval1.png'
-import Worddagi2 from './ram_jadval2.png'
-import Worddagi3 from './ram_jadval3.png'
-import Worddagi4 from './ram_jadval4.png'
-import Worddagi5 from './ram_jadval5.png'
+// import Worddagi1 from './ram_jadval1.png'
+// import Worddagi2 from './ram_jadval2.png'
+// import Worddagi3 from './ram_jadval3.png'
+// import Worddagi4 from './ram_jadval4.png'
+// import Worddagi5 from './ram_jadval5.png'
 
 
+import Worddagi1 from './v2.jpg'
+import Worddagi2 from './v3.jpg'
+import Worddagi3 from './v4.jpg'
+import Worddagi4 from './v5.jpg'
+import MyVideo from './w13.mp4'
 
 import MData from './rasmlar.zip'
 
@@ -35,15 +40,15 @@ function App() {
             ------------------------   ------------------------
             <img src={Worddagi4} alt=""/>
 
-            ------------------------   ------------------------
-            <img src={Worddagi5} alt=""/>
+            {/*------------------------   ------------------------*/}
+            {/*<img src={Worddagi5} alt=""/>*/}
             {/*------------------------Rasmlarni yuklab oling------------------------*/}
 
-            <a href={MData}>Rasmlarni yuklab oling</a>
-            {/*<video  controls>*/}
-            {/*    <source src={MyVideo} type="video/mp4" />*/}
-            {/*    Your browser does not support the video tag.*/}
-            {/*</video>*/}
+            {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
+            <video  controls>
+                <source src={MyVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
              </div>
 
 
