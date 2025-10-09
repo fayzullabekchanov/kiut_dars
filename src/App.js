09d2fd3a8e42e05aa9f1c6fa8fb80908ea9cbf6c
fayzullabekchanov <faiz0587@gmail.com>
2025-10-09 18:02:02 +0500

    Excel_1.1_

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 
-// import Worddagi1 from './EXCEL_BALL.png'
-import Worddagi1 from './ram_jadval1.png'
-import Worddagi2 from './ram_jadval2.png'
-import Worddagi3 from './ram_jadval3.png'
-import Worddagi4 from './ram_jadval4.png'
-import Worddagi5 from './ram_jadval5.png'
+import Worddagi1 from './EXCEL_BALL.png'
+// import Worddagi1 from './ram_jadval1.png'
+// import Worddagi2 from './ram_jadval2.png'
+// import Worddagi3 from './ram_jadval3.png'
+// import Worddagi4 from './ram_jadval4.png'
+// import Worddagi5 from './ram_jadval5.png'
 
 //
 // import Worddagi1 from './v2.jpg'
 // import Worddagi2 from './v3.jpg'
 // import Worddagi3 from './v4.jpg'
 // import Worddagi4 from './v5.jpg'
-import MyVideo from './w4.mp4'
+// import MyVideo from './w4.mp4'
 
-import MData from './rasmlar.zip'
+// import MData from './rasmlar.zip'
 //
 // import Worddagi1 from './excel1.png'
 // import Worddagi2 from './excel1.1.png'
@@ -37,24 +37,24 @@ function App() {
             {/*<h1>Faqat bitta variantini tanlang</h1>*/}
             <img src={Worddagi1} alt=""/>
             ------------------------   ------------------------
-            <img src={Worddagi2} alt="" />
+            {/*<img src={Worddagi2} alt="" />*/}
 
-            ------------------------   ------------------------
-            <img src={Worddagi3} alt=""/>
+            {/*------------------------   ------------------------*/}
+            {/*<img src={Worddagi3} alt=""/>*/}
 
-            ------------------------   ------------------------
-            <img src={Worddagi4} alt=""/>
+            {/*------------------------   ------------------------*/}
+            {/*<img src={Worddagi4} alt=""/>*/}
 
-            ------------------------   ------------------------
-            <img src={Worddagi5} alt=""/>
-            ------------------------------------------------
+            {/*------------------------   ------------------------*/}
+            {/*<img src={Worddagi5} alt=""/>*/}
+            {/*------------------------------------------------*/}
 
-            <a href={MData}>Rasmlarni yuklab oling</a>
-            ------------------------------------------------
-            <video  controls>
-                <source src={MyVideo} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
+            {/*------------------------------------------------*/}
+            {/*<video  controls>*/}
+            {/*    <source src={MyVideo} type="video/mp4" />*/}
+            {/*    Your browser does not support the video tag.*/}
+            {/*</video>*/}
 
         </div>
 
