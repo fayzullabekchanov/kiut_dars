5899688c00b53be4cbc7e77c43d2595de650f314
fayzullabekchanov <faiz0587@gmail.com>
2025-10-14 16:34:43 +0500

    Excel_1.1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 import Worddagi1 from './excel1.png'
 import Worddagi2 from './excel1.1.png'
+import Worddagi3 from './excel1.0.png'
 
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -19,11 +20,12 @@ function App() {
 
         <div className="App">
 
-            <img src={Worddagi1} alt=""/>
+            <img src={Worddagi1} alt="" width={1500}/>
             ---------------------------------
             <img src={Worddagi2} alt=""/>
-
-
+            ---------------------------------
+<h1>Rus tilida</h1>
+            <img src={Worddagi3} alt="" />
             {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
             {/*------------------------------------------------*/}
             {/*<video  controls>*/}
