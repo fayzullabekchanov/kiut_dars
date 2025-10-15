354325ae42fe928fdff406e8c1c20e8658ea1c25
fayzullabekchanov <faiz0587@gmail.com>
2025-10-15 09:04:21 +0500

    Excel_1.Variant

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 
 
-import Worddagi1 from './excel1.png'
-import Worddagi2 from './excel1.1.png'
-import Worddagi3 from './excel1.0.png'
+import Worddagi1 from './IMG_2644.jpg'
+import Worddagi2 from './excel1.png'
+import Worddagi3 from './EXCEL_BALL.png'
+import Worddagi4 from './excel6.png'
+
 
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -20,12 +22,13 @@ function App() {
 
         <div className="App">
 
-            <img src={Worddagi1} alt="" width={1500}/>
-            ---------------------------------
-            <img src={Worddagi2} alt=""/>
-            ---------------------------------
-<h1>Rus tilida</h1>
-            <img src={Worddagi3} alt="" />
+            <img src={Worddagi1}/>
+            ----------------------------------------------------------
+            <img src={Worddagi2} />
+            ----------------------------------------------------------
+            <img src={Worddagi3} />
+            ----------------------------------------------------------
+            <img src={Worddagi4} />
             {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
             {/*------------------------------------------------*/}
             {/*<video  controls>*/}
