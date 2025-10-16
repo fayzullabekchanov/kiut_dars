420a350c60e0ade8d29e2cf2fb63521bfdd835a2
fayzullabekchanov <faiz0587@gmail.com>
2025-10-16 18:32:33 +0500

    Excel_1.7_rus

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Worddagi2 from './excel1.png'
 import Worddagi3 from './EXCEL_BALL.png'
 import Worddagi4 from './excel6.png'
 import Worddagi5 from './excel2.png'
+import Worddagi6 from './excel7.png'
 import MyVideo from './Excel1.mp4'
 
 
@@ -38,6 +39,9 @@ function App() {
             <img src={Worddagi4}  alt="rasmbor" width={900} />
             ----------------------------------------------------------
             <img src={Worddagi5}  alt="rasmbor" width={900} />
+
+            ----------------------------------------------------------
+            <img src={Worddagi6}  alt="rasmbor" width={900} />
 
             {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
             {/*------------------------------------------------*/}
