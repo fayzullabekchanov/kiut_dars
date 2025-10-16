6e622326dac7b69e3105a9f86047547a9dd903be
fayzullabekchanov <faiz0587@gmail.com>
2025-10-16 18:00:44 +0500

    Excel_1.4_rus

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Worddagi1 from './excel1.0.png'
 import Worddagi2 from './excel1.png'
 import Worddagi3 from './EXCEL_BALL.png'
 import Worddagi4 from './excel6.png'
+import Worddagi5 from './excel2.png'
 import MyVideo from './Excel1.mp4'
 
 
@@ -25,16 +26,19 @@ function App() {
 
             <img src={Worddagi2} alt="rasmbor" width={900}/>
             ----------------------------------------------------------
-            <video  controls>
-                <source src={MyVideo}  type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            {/*<video  controls>*/}
+            {/*    <source src={MyVideo}  type="video/mp4" />*/}
+            {/*    Your browser does not support the video tag.*/}
+            {/*</video>*/}
 
             <img src={Worddagi1} alt="rasmbor" width={900} />
             ----------------------------------------------------------
             <img src={Worddagi3}  alt="rasmbor" width={900} />
             ----------------------------------------------------------
             <img src={Worddagi4}  alt="rasmbor" width={900} />
+            ----------------------------------------------------------
+            <img src={Worddagi5}  alt="rasmbor" width={900} />
+
             {/*<a href={MData}>Rasmlarni yuklab oling</a>*/}
             {/*------------------------------------------------*/}
 
