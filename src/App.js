a943633027a0d54cb830c4a264a85f313d2477d5
fayzullabekchanov <faiz0587@gmail.com>
2025-10-22 19:05:32 +0500

    Excel_1.2_ball_V

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi1 from './excel2.png'
+import Worddagi1 from './excel1.png'
+import Worddagi2 from './excel1.1.png'
+import MyVideo from './Excel1.mp4'
 
 
 
@@ -21,11 +23,12 @@ function App() {
         <div className="App">
 
             <img src={Worddagi1} alt="rasmbor" width={900}/>
+            <img src={Worddagi2} alt="rasmbor" width={900}/>
 
-            {/*<video  controls>*/}
-            {/*    <source src={MyVideo}  type="video/mp4" />*/}
-            {/*    Your browser does not support the video tag.*/}
-            {/*</video>*/}
+            <video  controls>
+                <source src={MyVideo}  type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
 
 
 
