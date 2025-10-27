e3ea0178773af32dd38d58b04d1b128dd798bb93
fayzullabekchanov <faiz0587@gmail.com>
2025-10-27 09:10:06 +0500

    Excel_1.2_ball_V_f_sistema_excel

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi1 from './excel8.png'
-import Worddagi2 from './1.PNG'
-import Worddagi3 from './2.PNG'
+import Worddagi1 from './excel3.1.png'
+
 
 
 
@@ -25,10 +24,7 @@ function App() {
 
             <img src={Worddagi1} alt="rasmbor" width={900}/>
             -------------------------------------------------------
-            <img src={Worddagi2} alt="rasmbor" width={900}/>
-            -------------------------------------------------------
-            <img src={Worddagi3} alt="rasmbor" width={900}/>
-            -------------------------------------------------------
+
             {/*<video  controls>*/}
             {/*    <source src={MyVideo}  type="video/mp4" />*/}
             {/*    Your browser does not support the video tag.*/}
