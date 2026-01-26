9c72277cf5416c70b8aaa49d2fbb30e5f8e2f821
fayzullabekchanov <faiz0587@gmail.com>
2026-01-26 18:27:37 +0500

    Triangel_python

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi1 from './1.PNG'
-import Worddagi2 from './2.PNG'
-import Worddagi3 from './3.png'
+import Worddagi1 from './triangel.png'
+
 
 
 
@@ -19,13 +18,8 @@ function App() {
         <div className="App">
 
 
-            <h1>Mushuk</h1>
-            <img src={Worddagi1} width={900} alt=""/>
-            <h1>Sichqon</h1>
 
-            <img src={Worddagi2} width={900} alt=""/>
-            <h1>Bombacha</h1>
-            <img src={Worddagi3} width={900} alt=""/>
+            <img src={Worddagi1} width={900} alt=""/>
             {/*<a href={Worddagi1}>Yuklab oling</a>*/}
 
             {/*<video  controls>*/}
