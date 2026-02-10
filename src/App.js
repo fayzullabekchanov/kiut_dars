80c1cb50158c87aaa1d4dde6b226e805fe331478
fayzullabekchanov <faiz0587@gmail.com>
2026-02-10 12:05:58 +0500

    Funksiya_misol_4

--- src/App.js
+++ src/App.js
@@ -3,40 +3,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-
-
-
-
-
-
-
+import Worddagi1 from './funksiya.png'
 function App() {
 
     return (
-
-
         <div className="App">
-
-
-
             <img src={Worddagi1} width={900} alt=""/>
-            -------------------------------------------------------
-
-
             {/*<a href={Worddagi1}>Yuklab oling</a>*/}
-
             {/*<video  controls>*/}
             {/*    <source src={MyVideo}  type="video/mp4" />*/}
             {/*    Your browser does not support the video tag.*/}
             {/*</video>*/}
-
-
-
-
         </div>
-
-
     );
 }
-
 export default App;
