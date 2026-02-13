85751c743f1bf4add06b5cb66de5715b6e7d8980
fayzullabekchanov <faiz0587@gmail.com>
2026-02-13 08:58:05 +0500

    Tarjimaiy_hol

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi1 from './funksiya.png'
-import Worddagi2 from './funksiya1.png'
+import Worddagi1 from './t1.png'
+import Worddagi2 from './t2.png'
+import Worddagi3 from './t3.png'
 function App() {
 
     return (
         <div className="App">
             <img src={Worddagi1} width={900} alt=""/>
-            -----------------------------------------------------------------------------------------------
             <img src={Worddagi2} width={900} alt=""/>
+            <img src={Worddagi3} width={900} alt=""/>
             {/*<a href={Worddagi1}>Yuklab oling</a>*/}
             {/*<video  controls>*/}
             {/*    <source src={MyVideo}  type="video/mp4" />*/}
