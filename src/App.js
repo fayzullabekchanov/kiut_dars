727d0d14d7b1914cb6d69e1669e663bd4cefa9cf
fayzullabekchanov <faiz0587@gmail.com>
2026-02-16 18:06:00 +0500

    M1-4

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi1 from './infograf.jpg'
-// import Worddagi2 from './obyektivka1.png'
-// import Worddagi3 from './obyektivka2.png'
-// import Worddagi4 from './obyektivka3.png'
+import Worddagi1 from './m1.png'
+import Worddagi2 from './m2.png'
+import Worddagi3 from './m3.png'
+import Worddagi4 from './m4.png'
 
 function App() {
 
     return (
         <div className="App">
             <img src={Worddagi1} width={900} alt=""/>
+            <img src={Worddagi2} width={900} alt=""/>
+            <img src={Worddagi3} width={900} alt=""/>
+            <img src={Worddagi4} width={900} alt=""/>
 
 
 
