cdd86cdaea479fbd207d24a056db5c6da2997433
fayzullabekchanov <faiz0587@gmail.com>
2026-02-20 11:00:10 +0500

    jadval

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi1 from './avto1.jpg'
-import Worddagi2 from './avto2.jpg'
+import Worddagi1 from './t_1.jpg'
+import Worddagi2 from './t_2.jpg'
+import Worddagi3 from './t_3.jpg'
+import Worddagi4 from './t_4.jpg'
+import Worddagi5 from './t_5.jpg'
 
 
 function App() {
@@ -12,7 +15,14 @@ function App() {
     return (
         <div className="App">
             <img src={Worddagi1} width={900} alt=""/>
+            ----------------------------------------------------------------------------------------------
             <img src={Worddagi2} width={900} alt=""/>
+            ----------------------------------------------------------------------------------------------
+            <img src={Worddagi3} width={900} alt=""/>
+            ----------------------------------------------------------------------------------------------
+            <img src={Worddagi4} width={900} alt=""/>
+            ----------------------------------------------------------------------------------------------
+            <img src={Worddagi5} width={900} alt=""/>
 
         </div>
     );
