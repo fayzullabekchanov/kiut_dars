69dfc1a3a0b3bc7c8ca1b9c3deb056d77af90d8c
fayzullabekchanov <faiz0587@gmail.com>
2026-02-20 13:47:00 +0500

    Obektivka_ped

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi1 from './t_1.jpg'
-import Worddagi2 from './t_2.jpg'
-import Worddagi3 from './t_3.jpg'
-import Worddagi4 from './t_4.jpg'
-import Worddagi5 from './t_5.jpg'
+
+import Worddagi1 from './obyektivka1.png'
+import Worddagi2 from './obyektivka2.png'
+import Worddagi3 from './obyektivka3.png'
+
+
+// import Worddagi1 from './t_1.jpg'
+// import Worddagi2 from './t_2.jpg'
+// import Worddagi3 from './t_3.jpg'
+// import Worddagi4 from './t_4.jpg'
+// import Worddagi5 from './t_5.jpg'
 
 function App() {
 
@@ -18,10 +24,10 @@ function App() {
             <img src={Worddagi2} width={900} alt=""/>
             ----------------------------------------------------------------------------------------------
             <img src={Worddagi3} width={900} alt=""/>
-            ----------------------------------------------------------------------------------------------
-            <img src={Worddagi4} width={900} alt=""/>
-            ----------------------------------------------------------------------------------------------
-            <img src={Worddagi5} width={900} alt=""/>
+            {/*----------------------------------------------------------------------------------------------*/}
+            {/*<img src={Worddagi4} width={900} alt=""/>*/}
+            {/*----------------------------------------------------------------------------------------------*/}
+            {/*<img src={Worddagi5} width={900} alt=""/>*/}
 
         </div>
     );
