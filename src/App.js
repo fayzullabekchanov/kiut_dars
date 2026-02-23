7ef9bd12e8bad51e460555f5d2ae9353cdb400ae
fayzullabekchanov <faiz0587@gmail.com>
2026-02-23 17:29:45 +0500

    avto2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 
-import Worddagi1 from './obyektivka1.png'
-import Worddagi2 from './obyektivka2.png'
-import Worddagi3 from './obyektivka3.png'
+// import Worddagi1 from './obyektivka1.png'
+// import Worddagi2 from './obyektivka2.png'
+// import Worddagi3 from './obyektivka3.png'
 
 
+import Worddagi1 from './avto2.jpg'
 // import Worddagi1 from './t_1.jpg'
 // import Worddagi2 from './t_2.jpg'
 // import Worddagi3 from './t_3.jpg'
@@ -19,14 +20,10 @@ function App() {
 
     return (
         <div className="App">
-            <img src={Worddagi1} width={900} alt=""/>
-            ----------------------------------------------------------------------------------------------
-            <img src={Worddagi2} width={900} alt=""/>
-            ----------------------------------------------------------------------------------------------
-            <img src={Worddagi3} width={900} alt=""/>
-            {/*----------------------------------------------------------------------------------------------*/}
-            {/*<img src={Worddagi4} width={900} alt=""/>*/}
-            {/*----------------------------------------------------------------------------------------------*/}
+            <img src={Worddagi1} width={900} alt=""/>----------------------------------------------------------------------------------------------
+            {/*<img src={Worddagi2} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
+            {/*<img src={Worddagi3} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
+            {/*<img src={Worddagi4} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
             {/*<img src={Worddagi5} width={900} alt=""/>*/}
 
         </div>
