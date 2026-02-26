0e0443193b01cf01a2b46fc16842fc62e8afbb47
fayzullabekchanov <faiz0587@gmail.com>
2026-02-26 10:19:33 +0500

    avto_t_2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 
 
 import Worddagi1 from './oopt1.jpg'
-// import Worddagi1 from './t_1.jpg'
+import Worddagi2 from './oopt2.jpg'
 // import Worddagi2 from './t_2.jpg'
 // import Worddagi3 from './t_3.jpg'
 // import Worddagi4 from './t_4.jpg'
@@ -21,7 +21,7 @@ function App() {
     return (
         <div className="App">
             <img src={Worddagi1} width={900} alt=""/>----------------------------------------------------------------------------------------------
-            {/*<img src={Worddagi2} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
+            <img src={Worddagi2} width={900} alt=""/>----------------------------------------------------------------------------------------------
             {/*<img src={Worddagi3} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
             {/*<img src={Worddagi4} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
             {/*<img src={Worddagi5} width={900} alt=""/>*/}
