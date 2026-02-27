c1e64c527a776ab16b6ccffc4052faaf61427811
fayzullabekchanov <faiz0587@gmail.com>
2026-02-27 15:00:47 +0500

    word_1_2_f

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import Worddagi1 from './word1.jpg'
 import Worddagi2 from './word2.jpg'
+import Worddagi3 from './formula.png'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <div className="App">
             <img src={Worddagi1} width={900} alt=""/>----------------------------------------------------------------------------------------------
             <img src={Worddagi2} width={900} alt=""/>----------------------------------------------------------------------------------------------
+            <img src={Worddagi3} width={900} alt=""/>----------------------------------------------------------------------------------------------
         </div>
     );
 }
