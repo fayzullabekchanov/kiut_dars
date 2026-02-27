7a71acf46e44355b66ba44d3ad2824271f8b6896
fayzullabekchanov <faiz0587@gmail.com>
2026-02-27 11:05:39 +0500

    word_nazorat

--- src/App.js
+++ src/App.js
@@ -4,28 +4,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 
-// import Worddagi1 from './obyektivka1.png'
-// import Worddagi2 from './obyektivka2.png'
-// import Worddagi3 from './obyektivka3.png'
 
-
-import Worddagi1 from './word1.jpg'
-import Worddagi2 from './word2.jpg'
 import Worddagi3 from './formula.png'
-// import Worddagi2 from './t_2.jpg'
-// import Worddagi3 from './t_3.jpg'
-// import Worddagi4 from './t_4.jpg'
-// import Worddagi5 from './t_5.jpg'
+import Worddagi4 from './a.jpg'
+import Worddagi5 from './b.jpg'
+import Worddagi6 from './c.jpg'
+import Worddagi7 from './d.jpg'
+
 
 function App() {
 
     return (
         <div className="App">
-            <img src={Worddagi1} width={900} alt=""/>----------------------------------------------------------------------------------------------
-            <img src={Worddagi2} width={900} alt=""/>----------------------------------------------------------------------------------------------
+
             <img src={Worddagi3} width={900} alt=""/>----------------------------------------------------------------------------------------------
-            {/*<img src={Worddagi4} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
-            {/*<img src={Worddagi5} width={900} alt=""/>*/}
+
+            <img src={Worddagi4} width={900} alt=""/>----------------------------------------------------------------------------------------------
+            <img src={Worddagi5} width={900} alt=""/>----------------------------------------------------------------------------------------------
+            <img src={Worddagi6} width={900} alt=""/>----------------------------------------------------------------------------------------------
+            <img src={Worddagi7} width={900} alt=""/>----------------------------------------------------------------------------------------------
 
         </div>
     );
