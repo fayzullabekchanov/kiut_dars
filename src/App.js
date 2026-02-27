f31ae65402b61267a3924cd1554900d54f3af8d8
fayzullabekchanov <faiz0587@gmail.com>
2026-02-27 13:52:19 +0500

    word_1_2

--- src/App.js
+++ src/App.js
@@ -3,30 +3,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-
-
 import Worddagi1 from './word1.jpg'
 import Worddagi2 from './word2.jpg'
-// import Worddagi3 from './formula.png'
-// import Worddagi4 from './a.jpg'
-// import Worddagi5 from './b.jpg'
-// import Worddagi6 from './c.jpg'
-// import Worddagi7 from './d.jpg'
-
 
 function App() {
 
     return (
         <div className="App">
-
             <img src={Worddagi1} width={900} alt=""/>----------------------------------------------------------------------------------------------
             <img src={Worddagi2} width={900} alt=""/>----------------------------------------------------------------------------------------------
-            {/*<img src={Worddagi3} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
-            {/*<img src={Worddagi4} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
-            {/*<img src={Worddagi5} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
-            {/*<img src={Worddagi6} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
-            {/*<img src={Worddagi7} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
-
         </div>
     );
 }
