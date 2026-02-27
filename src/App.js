dca502a8c49e1b3f9113fb0dd2328cb8f207040b
fayzullabekchanov <faiz0587@gmail.com>
2026-02-27 10:06:49 +0500

    word_grafika_formula

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 
 import Worddagi1 from './word1.jpg'
 import Worddagi2 from './word2.jpg'
+import Worddagi3 from './formula.png'
 // import Worddagi2 from './t_2.jpg'
 // import Worddagi3 from './t_3.jpg'
 // import Worddagi4 from './t_4.jpg'
@@ -22,7 +23,7 @@ function App() {
         <div className="App">
             <img src={Worddagi1} width={900} alt=""/>----------------------------------------------------------------------------------------------
             <img src={Worddagi2} width={900} alt=""/>----------------------------------------------------------------------------------------------
-            {/*<img src={Worddagi3} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
+            <img src={Worddagi3} width={900} alt=""/>----------------------------------------------------------------------------------------------
             {/*<img src={Worddagi4} width={900} alt=""/>----------------------------------------------------------------------------------------------*/}
             {/*<img src={Worddagi5} width={900} alt=""/>*/}
 
