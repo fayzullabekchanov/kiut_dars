08e87fe2235616a65aa153ec7ecabc292cf158bb
fayzullabekchanov <faiz0587@gmail.com>
2026-03-06 13:45:34 +0500

    Excel1.0

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi3 from './Excel2_xar.jpg'
-import Worddagi4 from './Excel2_xar1.jpg'
-import Worddagi5 from './Excel2_ombor.jpg'
+import Worddagi3 from './excel1.0.png'
+import Worddagi4 from './excel1.1.jpg'
 
 
 function App() {
 
     return (
         <div className="App">
-            <img src={Worddagi3} alt=""/>
+            <img src={Worddagi3} alt=""/>----------------------------
             <img src={Worddagi4} alt=""/>----------------------------
 
-                <img src={Worddagi5} alt=""/>----------------------------
         </div>
     );
 }
