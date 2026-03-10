881072ba28701f1ef23abf952769c37ebb668b66
fayzullabekchanov <faiz0587@gmail.com>
2026-03-10 10:07:20 +0500

    TRM

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Worddagi3 from './Excel2_xar.jpg'
-import Worddagi4 from './Excel2_xar1.jpg'
-import Worddagi5 from './Excel2_ombor.jpg'
+import Worddagi3 from './TRM.png'
 
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
     return (
         <div className="App">
             <img src={Worddagi3} alt=""/>
-            <img src={Worddagi4} alt=""/>----------------------------------------------------------------------------------------------------------------
-            <img src={Worddagi5} alt=""/>----------------------------------------------------------------------------------------------------------------
 
         </div>
     );
