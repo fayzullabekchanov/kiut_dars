909250b920a26fe237c8a53eea19b2bb3b930168
fayzullabekchanov <faiz0587@gmail.com>
2026-03-12 09:52:56 +0500

    TRM1_2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     return (
         <div className="App">
 
-            <a href="https://download-cdn.jetbrains.com/python/pycharm-2025.3.3.exe">PayCharmni o`rnating</a>
+            {/*<a href="https://download-cdn.jetbrains.com/python/pycharm-2025.3.3.exe">PayCharmni o`rnating</a>*/}
             <img src={Worddagi3} alt=""/>
 
         </div>
