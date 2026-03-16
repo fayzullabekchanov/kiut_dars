162424daf5f3d47a04426c5af0811eeb5248fb3c
fayzullabekchanov <faiz0587@gmail.com>
2026-03-16 17:49:40 +0500

    TRM1_2

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 import Worddagi1 from './TRM.png'
-// import Worddagi2 from './excel3_qadam2.png'
-// import Worddagi3 from './excel3_qadam3.png'
-// import Worddagi4 from './excel3_qadam4.png'
-// import Worddagi5 from './excel3_natija.png'
+
 
 
 function App() {
@@ -15,20 +12,10 @@ function App() {
     return (
         <div className="App">
 
-           {/*<h1>1-qadam Excelda shu jadvallarni yaratamiz </h1>*/}
+
             <img src={Worddagi1} alt=""/>
 
-            {/*<h1>2-qadam jadvalga formulalarni kiritamiz </h1>*/}
-            {/*<img src={Worddagi2} alt=""/>*/}
 
-            {/*<h1>3-qadam jadvalga formulalarni kiritamiz </h1>*/}
-            {/*<img src={Worddagi3} alt=""/>*/}
-
-            {/*<h1>4-qadam qizil bilan belgilangan jadvaldagi kataklarga formulalarni kiritamiz </h1>*/}
-            {/*<img src={Worddagi4} alt=""/>*/}
-
-            {/*<h1>Sizning natijangiz quyidagicha bo`lishi kerak </h1>*/}
-            {/*<img src={Worddagi5} alt=""/>*/}
 
         </div>
     );
