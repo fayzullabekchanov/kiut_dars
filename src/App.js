adf440ae78ba9f862d5b61ba21ae0cd7606db2cd
fayzullabekchanov <faiz0587@gmail.com>
2026-03-30 17:57:34 +0500

    TRM_Json2_1_1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from './TRM.png';
+import Rasm1 from './Masala_Json_Page_1.jpg';
+import Rasm2 from './Masala_Json_Page_2.jpg';
+import Rasm3 from './Masala_Json_Page_3.jpg';
 
 
 
@@ -14,6 +16,8 @@ function App() {
         <div className="App">
 
             <img src={Rasm1} alt=""/>
+            <img src={Rasm2} alt=""/>
+            <img src={Rasm3} alt=""/>
 
         </div>
     );
