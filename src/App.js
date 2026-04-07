43547cff732cc46270bcd2c846453fb0b8755d1b
fayzullabekchanov <faiz0587@gmail.com>
2026-04-07 09:49:37 +0500

    TRM_Forma

--- src/App.js
+++ src/App.js
@@ -3,25 +3,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from './1.png';
-import Rasm2 from './2.png';
-import Rasm3 from './3.png';
-import Rasm4 from './4.png';
-import Rasm5 from './5.png';
-
-
-
-
+import Rasm1 from './pifagor.png';
 function App() {
 
     return (
         <div className="App">
 
             <img src={Rasm1} alt=""/>
-            <img src={Rasm2} alt=""/>
-            <img src={Rasm3} alt=""/>
-            <img src={Rasm4} alt=""/>
-            <img src={Rasm5} alt=""/>
 
         </div>
     );
