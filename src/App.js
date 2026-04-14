cd7e5679414be12de2a209809977fa980828a773
fayzulla <faiz0587@gmail.com>
2026-04-14 07:48:57 +0500

    TRM_F_s_GUI

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from './pifagor.png';
+import Rasm1 from './regs1.png';
+import Rasm2 from './regs2.png';
 function App() {
 
     return (
         <div className="App">
 
             <img src={Rasm1} alt=""/>
+            <img src={Rasm2} alt=""/>
 
         </div>
     );
