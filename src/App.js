55e05c4f4a04b2cf08b7fc630e776b4cf3fea5bf
fayzullabekchanov <faiz0587@gmail.com>
2026-04-17 08:50:50 +0500

    Google

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from './regs1.png';
-import Rasm2 from './regs2.png';
+import Rasm1 from './gogole.png';
+
 function App() {
 
     return (
         <div className="App">
 
             <img src={Rasm1} alt=""/>
-            <img src={Rasm2} alt=""/>
+
 
         </div>
     );
