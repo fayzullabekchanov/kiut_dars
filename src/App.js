ab498ab03afad57dcb27cc8c8079baed5fa58e10
fayzullabekchanov <faiz0587@gmail.com>
2026-04-21 10:33:02 +0500

    Scrat_sichqon_mushuk

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from './regs1.png';
-import Rasm2 from './regs2.png';
+import Rasm1 from './1.png';
+import Rasm2 from './2.png';
+import Rasm3 from './3.png';
 
 function App() {
 
     return (
         <div className="App">
 
-            <img src={Rasm1} alt=""/>
-            <img src={Rasm2} alt=""/>
+            <h1>Mushuk</h1>
+            <img src={Rasm1} width={500} alt=""/>
+            <h1>Sichqon</h1>
+            <img src={Rasm2} width={500} alt=""/>
+            <h1>Bomba</h1>
+            <img src={Rasm3} width={500} alt=""/>
 
 
         </div>
