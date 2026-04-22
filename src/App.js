b6fb6f2f461f2b235ca77342c120b021db4b8a2e
fayzullabekchanov <faiz0587@gmail.com>
2026-04-22 17:07:14 +0500

    v1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from './1.png';
-import Rasm2 from './2.png';
-import Rasm3 from './3.png';
+import Rasm1 from './v1.png';
+
 
 function App() {
 
     return (
         <div className="App">
 
-            <h1>Mushuk</h1>
-            <img src={Rasm1} width={500} alt=""/>
-            <h1>Sichqon</h1>
-            <img src={Rasm2} width={500} alt=""/>
-            <h1>Bomba</h1>
-            <img src={Rasm3} width={500} alt=""/>
+           <img src={Rasm1} width={500} alt=""/>
+
 
 
         </div>
