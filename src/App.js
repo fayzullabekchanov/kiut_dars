9605b236e49549df0832865690d5c7d88b08f7c3
fayzullabekchanov <faiz0587@gmail.com>
2026-04-28 11:52:45 +0500

    scratch_Likopcha

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from '././V1_Page_1.jpg';
-import Rasm2 from '././V1_Page_2.jpg';
-import Rasm3 from '././V1_Page_3.jpg';
+import Rasm1 from './l1.png';
+import Rasm2 from './l2.png';
 
 function App() {
 
     return (
         <div className="App">
-
-           <img src={Rasm1} width={500} alt=""/>
-           <img src={Rasm2} width={500} alt=""/>
-           <img src={Rasm3} width={500} alt=""/>
-
-
-
+            <h1>Likopcha</h1>
+            <img src={Rasm1} width={500} alt=""/>
+            <h1>Olma</h1>
+            <img src={Rasm2} width={500} alt=""/>
         </div>
     );
 }
+
 export default App;
