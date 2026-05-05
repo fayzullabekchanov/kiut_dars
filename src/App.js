6bacd88f117415713df10a67c808ee554ea3dd00
fayzullabekchanov <faiz0587@gmail.com>
2026-05-05 17:08:54 +0500

    TRM_YaN_V3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from './v4.png';
+import Rasm1 from './v3.png';
+import Rasm2 from './v3_1.png';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <div className="App">
 
             <img src={Rasm1} width={500} alt=""/>
+            <img src={Rasm2} width={500} alt=""/>
         </div>
     );
 }
