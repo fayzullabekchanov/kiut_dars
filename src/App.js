3f129f86dd98267716e8758dea972c593992b7a4
fayzullabekchanov <faiz0587@gmail.com>
2026-09-08 11:31:40 +0500

    Obektivka

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Rasm1 from './statisv.jpg';
+import Rasm1 from './obyektivka1.png';
+import Rasm2 from './obyektivka2.png';
+import Rasm3 from './obyektivka3.png';
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
         <div className="App">
 
             <img src={Rasm1} width={500} alt=""/>
+            <img src={Rasm2} width={500} alt=""/>
+            <img src={Rasm3} width={500} alt=""/>
         </div>
     );
 }
