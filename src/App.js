90e87324211bf9c4606c59bdc4f2ec6447802e1d
fayzullabekchanov <faiz0587@gmail.com>
2026-09-15 15:20:10 +0500

    Word_TablisaUZ

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 
-import Rasm1 from './tfw.png';
-// import Rasm1 from './t_1.jpg';
-// import Rasm2 from './t_2.jpg';
-// import Rasm3 from './t_3.jpg';
-// import Rasm4 from './t_4.jpg';
-// import Rasm5 from './t_5.jpg';
+// import Rasm1 from './tfw.png';
+import Rasm1 from './t_1.jpg';
+import Rasm2 from './t_2.jpg';
+import Rasm3 from './t_3.jpg';
+import Rasm4 from './t_4.jpg';
+import Rasm5 from './t_5.jpg';
 
 function App() {
 
     return (
         <div className="App">
             <img src={Rasm1} width={500} alt=""/>
-            {/*<img src={Rasm2} width={500} alt=""/>*/}
-            {/*<img src={Rasm3} width={500} alt=""/>*/}
-            {/*<img src={Rasm4} width={500} alt=""/>*/}
-            {/*<img src={Rasm5} width={500} alt=""/>*/}
+            <img src={Rasm2} width={500} alt=""/>
+            <img src={Rasm3} width={500} alt=""/>
+            <img src={Rasm4} width={500} alt=""/>
+            <img src={Rasm5} width={500} alt=""/>
         </div>
     );
 }
