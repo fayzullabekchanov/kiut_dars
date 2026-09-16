2a84878eea14974e4bf6e7c25d9d2b45956d853d
fayzullabekchanov <faiz0587@gmail.com>
2026-09-16 14:26:38 +0500

    Word_Table_Shapes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import Rasm2 from './tfw_rus.jpg';
 import Rasm3 from './word1.jpg';
 import Rasm4 from './word2.jpg';
 
+import Table1 from './t_1.jpg';
+import Table2 from './t_2.jpg';
+import Table3 from './t_3.jpg';
+import Table4 from './t_4.jpg';
+import Table5 from './t_5.jpg';
+
 function App() {
 
     return (
@@ -35,7 +41,90 @@ function App() {
                         <html lang="uz">
                         <head>
                             <meta charset="UTF-8">
-                            <title>IST-901U</title>
+                            <title>Shapes</title>
+                        </head>
+
+                        <body style="
+                            margin: 0;
+                            min-height: 100vh;
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
+                            justify-content: center;
+                            gap: 20px;
+                        ">
+
+                            <img 
+                                src="${Table1}" 
+                                alt="IST-901U - Topshiriq 1"
+                                style="
+                                    max-width: 80%;
+                                    max-height: 80vh;
+                                    object-fit: contain;
+                                "
+                            >
+
+                            <img 
+                                src="${Table2}" 
+                                alt="IST-901U - Topshiriq 2"
+                                style="
+                                    max-width: 80%;
+                                    max-height: 80vh;
+                                    object-fit: contain;
+                                "
+                            >
+                            <img 
+                                src="${Table3}" 
+                                alt="IST-901U - Topshiriq 2"
+                                style="
+                                    max-width: 80%;
+                                    max-height: 80vh;
+                                    object-fit: contain;
+                                "
+                            >
+                            <img 
+                                src="${Table4}" 
+                                alt="IST-901U - Topshiriq 2"
+                                style="
+                                    max-width: 80%;
+                                    max-height: 80vh;
+                                    object-fit: contain;
+                                "
+                            >
+                            <img 
+                                src="${Table5}" 
+                                alt="IST-901U - Topshiriq 2"
+                                style="
+                                    max-width: 80%;
+                                    max-height: 80vh;
+                                    object-fit: contain;
+                                "
+                            >
+
+                        </body>
+                        </html>
+                    `);
+
+                    yangiOyna.document.close();
+                }}
+            >
+                Table
+            </a>
+            <a
+                href={Rasm3}
+                target="_blank"
+                rel="noreferrer"
+                onClick={(e) => {
+                    e.preventDefault();
+
+                    const yangiOyna = window.open('', '_blank');
+
+                    yangiOyna.document.write(`
+                        <!DOCTYPE html>
+                        <html lang="uz">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Shapes</title>
                         </head>
 
                         <body style="
@@ -75,7 +164,7 @@ function App() {
                     yangiOyna.document.close();
                 }}
             >
-                IST-901U
+                Shapes
             </a>
 
         </div>
