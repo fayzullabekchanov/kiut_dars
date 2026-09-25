040142748c7962de226c6ab255338faf916e4284
fayzullabekchanov <faiz0587@gmail.com>
2026-09-25 11:43:22 +0500

    Word_Table_Shapes_Formula

--- src/App.js
+++ src/App.js
@@ -16,17 +16,68 @@ import Table3 from './t_3.jpg';
 import Table4 from './t_4.jpg';
 import Table5 from './t_5.jpg';
 
+import TableFormula1 from './table1.png';
+import TableFormula2 from './table1rus.png';
+
+
+import TableImages1 from './ram_jadval1.png';
+import TableImages2 from './ram_jadval2.png';
+import TableImages3 from './ram_jadval3.png';
+import TableImages4 from './ram_jadval4.png';
+import TableImages5 from './ram_jadval5.png';
+
+
+
 function App() {
 
     return (
         <div className="App">
 
-            <a href={Rasm1} target="_blank" rel="noreferrer">
-                Uzbek
-            </a>
+            <a
+                href={TableImages1}
+                target="_blank"
+                rel="noreferrer"
+                onClick={(e) => {
+                    e.preventDefault();
 
-            <a href={Rasm2} target="_blank" rel="noreferrer">
-                Rus tili
+                    const yangiOyna = window.open('', '_blank');
+
+                    yangiOyna.document.write(`
+                        <!DOCTYPE html>
+                        <html lang="uz">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>  Text Color</title>
+                        </head>
+                           
+                         
+                           
+                           
+                        <body style="
+                            margin: 0;
+                            min-height: 100vh;
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
+                            justify-content: center;
+                            /*gap: 20px;*/
+                        ">
+<h1>Uzbek tilida</h1>
+                            <img  src="${Rasm1}"   alt="Topshiriq" >
+                            <h1>Rus tilida</h1>    
+                            <img  src="${Rasm2}"   alt="Topshiriq" >    
+                            
+                            
+                                                   
+
+                        </body>
+                        </html>
+                    `);
+
+                    yangiOyna.document.close();
+                }}
+            >
+                Text Color
             </a>
 
             <a
@@ -45,7 +96,7 @@ function App() {
                             <meta charset="UTF-8">
                             <title>Table</title>
                         </head>
-
+                          
                         <body style="
                             margin: 0;
                             min-height: 100vh;
@@ -215,6 +266,112 @@ function App() {
             >
                 Formulalar
             </a>
+
+            <a
+                href={TableFormula1}
+                target="_blank"
+                rel="noreferrer"
+                onClick={(e) => {
+                    e.preventDefault();
+
+                    const yangiOyna = window.open('', '_blank');
+
+                    yangiOyna.document.write(`
+                        <!DOCTYPE html>
+                        <html lang="uz">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title> Table Formula</title>
+                        </head>
+
+                        <body style="
+                            margin: 0;
+                            min-height: 100vh;
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
+                            justify-content: center;
+                            gap: 20px;
+                        ">
+
+                            <img 
+                                src="${TableFormula1}" 
+                                alt="IST-901U - Topshiriq 1"
+                                style="
+                                    max-width: 80%;
+                                    max-height: 80vh;
+                                    object-fit: contain;
+                                "
+                            >    
+                            
+                             <img 
+                                src="${TableFormula2}" 
+                                alt="IST-901U - Topshiriq 1"
+                                style="
+                                    max-width: 80%;
+                                    max-height: 80vh;
+                                    object-fit: contain;
+                                "
+                            >                          
+
+                        </body>
+                        </html>
+                    `);
+
+                    yangiOyna.document.close();
+                }}
+            >
+                Table Formula
+            </a>
+            <a
+                href={TableImages1}
+                target="_blank"
+                rel="noreferrer"
+                onClick={(e) => {
+                    e.preventDefault();
+
+                    const yangiOyna = window.open('', '_blank');
+
+                    yangiOyna.document.write(`
+                        <!DOCTYPE html>
+                        <html lang="uz">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>  Table Images</title>
+                        </head>
+                           
+                           <a href="/rasmlar.zip" download="rasmlar.zip" className="btn btn-primary">
+    Rasmlar ZIP faylini yuklab olish </a>
+                           
+                           
+                        <body style="
+                            margin: 0;
+                            min-height: 100vh;
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
+                            justify-content: center;
+                            /*gap: 20px;*/
+                        ">
+
+                            <img  src="${TableImages1}"   alt="Topshiriq" >    
+                            <img  src="${TableImages2}"   alt="Topshiriq" >    
+                            <img  src="${TableImages3}"   alt="Topshiriq" >    
+                            <img  src="${TableImages4}"   alt="Topshiriq" >    
+                            <img  src="${TableImages5}"   alt="Topshiriq" >    
+                            
+                                                   
+
+                        </body>
+                        </html>
+                    `);
+
+                    yangiOyna.document.close();
+                }}
+            >
+                Table Images
+            </a>
+
 
         </div>
     );
